ffddc7b7af0071a0babbba3dddffd022c5013405
paninaro95mx <59038202+paninaro95mx@users.noreply.github.com>
2022-08-17 13:15:00 -0500

    first commit

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("assembler.com.mx.onchangetraffic.controller.App", {
+            onInit: function () {
+
+            }
+        });
+    });
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"assemblercommx/onchangetraffic/test/unit/controller/App.controller"
+], function () {
+	"use strict";
+});
