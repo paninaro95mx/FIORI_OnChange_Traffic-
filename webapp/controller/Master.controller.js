350d53bc0f5ef8c9b2b9973b6a84f3157dfcc563
paninaro95mx <59038202+paninaro95mx@users.noreply.github.com>
2022-08-23 15:51:35 -0500

    App ver 1.1

--- webapp/controller/Master.controller.js
+++ webapp/controller/Master.controller.js
@@ -1,19 +1,66 @@
 sap.ui.define(['sap/ui/core/mvc/Controller',
-               'sap/ui/model/json/JSONModel'],
-	function(Controller, JSONModel) {
+               'sap/ui/model/json/JSONModel',
+			   'sap/m/library',
+			   'assembler/com/mx/onchangetraffic/formatter/Formatter'
+			],
+	function(Controller, JSONModel, mobileLibrary, Formatter) {
 	"use strict";
+
+	var PopinLayout = mobileLibrary.PopinLayout;
 
 	return Controller.extend("assembler.com.mx.onchangetraffic.controller.Master", {
 
+		
+//			   
 		onInit: function () {
-			var oModel = new JSONModel({ data: {} });
-			this.getView().setModel(oModel);
+			console.log("onInit() of controller called...");
+            var oProductModel = new sap.ui.model.json.JSONModel();
+            oProductModel.loadData("./model/Products.json");
+			this.getView().setModel(oProductModel);
 		},
 
-		onLiveChange: function (oEvent) {
-			var sNewValue = oEvent.getParameter("value");
-			this.byId("getValue").setText(sNewValue);
-		}
+		onPopinLayoutChanged: function() {
+			var oTable = this.byId("mvcAppComponent---idMasterView--idProductsTable");
+			console.log(oTable);
+			var oComboBox = this.byId("idPopinLayout");
+			var sPopinLayout = oComboBox.getSelectedKey();
+			console.log(sPopinLayout);
+			switch (sPopinLayout) {
+				case "Block":
+					oTable.setPopinLayout(PopinLayout.Block);
+					break;
+				case "GridLarge":
+					oTable.setPopinLayout(PopinLayout.GridLarge);
+					break;
+				case "GridSmall":
+					oTable.setPopinLayout(PopinLayout.GridSmall);
+					break;
+				default:
+					oTable.setPopinLayout(PopinLayout.Block);
+					break;
+			}
+		},
 
+		onSelect: function(oEvent) {
+			var bSelected = oEvent.getParameter("selected"),
+				sText = oEvent.getSource().getText(),
+				oTable = this.byId("mvcAppComponent---idMasterView--idProductsTable"),
+				aSticky = oTable.getSticky() || [];
+
+			if (bSelected) {
+				aSticky.push(sText);
+			} else if (aSticky.length) {
+				var iElementIndex = aSticky.indexOf(sText);
+				aSticky.splice(iElementIndex, 1);
+			}
+
+			oTable.setSticky(aSticky);
+		},
+
+		onToggleInfoToolbar: function(oEvent) {
+			var oTable = this.byId("idProductsTable");
+			oTable.getInfoToolbar().setVisible(!oEvent.getParameter("pressed"));
+		}
+			
 	});
 });
